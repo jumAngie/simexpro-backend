b4fae46d013c27f56f3987ffbc108734476d1901
Christopher Aguilar <103384328+ChrizBBX@users.noreply.github.com>
2023-09-04 10:26:40 -0600

    Update BoletinPagoDetallesRepository.cs

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Adua/BoletinPagoDetallesRepository.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Adua/BoletinPagoDetallesRepository.cs
@@ -63,13 +63,13 @@ namespace SIMEXPRO.DataAccess.Repositories.Adua
 
             var parametros = new DynamicParameters();
             parametros.Add("@bode_Id", item.bode_Id, DbType.Int32, ParameterDirection.Input);
-            parametros.Add("@boen_Id", item.boen_Id, DbType.Int32, ParameterDirection.Input);
+            //parametros.Add("@boen_Id", item.boen_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@lige_Id", item.lige_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@bode_Concepto", item.bode_Concepto, DbType.String, ParameterDirection.Input);
             parametros.Add("@bode_TipoObligacion", item.bode_TipoObligacion, DbType.String, ParameterDirection.Input);
             parametros.Add("@bode_CuentaPA01", item.bode_CuentaPA01, DbType.Int32, ParameterDirection.Input);
-            parametros.Add("@usua_UsuarioCreacion", item.usua_UsuarioCreacion, DbType.Int32, ParameterDirection.Input);
-            parametros.Add("@bode_FechaCreacion", item.bode_FechaCreacion, DbType.DateTime, ParameterDirection.Input);
+            parametros.Add("@usua_UsuarioModificacion", item.usua_UsuarioModificacion, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@bode_FechaModificacion", item.bode_FechaModificacion, DbType.DateTime, ParameterDirection.Input);
 
             var respuesta = db.QueryFirst<string>(ScriptsDataBase.EditarBoletinPagoDetalles, parametros, commandType: CommandType.StoredProcedure);
 
