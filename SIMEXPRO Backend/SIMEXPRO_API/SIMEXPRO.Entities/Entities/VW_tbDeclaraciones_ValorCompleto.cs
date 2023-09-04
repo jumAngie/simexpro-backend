b3da3d43dab0f292d783f9dcff1a652e5cb74043
Axel Rivera <125411077+AxelRivera504@users.noreply.github.com>
2023-09-04 14:09:58 -0600

    Cambios vista declaracion de valor

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.Entities/Entities/VW_tbDeclaraciones_ValorCompleto.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.Entities/Entities/VW_tbDeclaraciones_ValorCompleto.cs
@@ -16,11 +16,29 @@ namespace SIMEXPRO.Entities.Entities
         public string deva_DeclaracionMercancia { get; set; }
         public DateTime? deva_FechaAceptacion { get; set; }
         public bool? deva_Finalizacion { get; set; }
-        public int? impo_Id { get; set; }
-        public string impo_NumRegistro { get; set; }
+        public bool? deva_PagoEfectuado { get; set; }
+        public int? pais_ExportacionId { get; set; }
+        public string pais_ExportacionNombre { get; set; }
+        public DateTime? deva_FechaExportacion { get; set; }
+        public int? mone_Id { get; set; }
+        public string mone_Otra { get; set; }
+        public decimal? deva_ConversionDolares { get; set; }
+
+
+        public int? emba_Id { get; set; }
+        public string LugarEmbarque { get; set; }
+
+
         public int? nico_Id { get; set; }
         public string nico_Descripcion { get; set; }
+
+
+        public int? impo_Id { get; set; }
+        public string impo_NumRegistro { get; set; }
+        public string impo_RTN { get; set; }
         public string impo_NivelComercial_Otro { get; set; }
+
+
         public string impo_Nombre_Raso { get; set; }
         public string impo_Direccion_Exacta { get; set; }
         public string impo_Correo_Electronico { get; set; }
@@ -28,7 +46,13 @@ namespace SIMEXPRO.Entities.Entities
         public string impo_Fax { get; set; }
         public int? impo_ciudId { get; set; }
         public int? impo_paisId { get; set; }
-        public string impo_RTN { get; set; }
+
+
+        public int? coco_Id { get; set; }
+        public string coco_Descripcion { get; set; }
+        public string pvde_Condicion_Otra { get; set; } 
+
+
         public int? pvde_Id { get; set; }
         public string prov_NumeroIdentificacion { get; set; }
         public string prov_Nombre_Raso { get; set; }
@@ -38,20 +62,27 @@ namespace SIMEXPRO.Entities.Entities
         public string prov_Fax { get; set; }
         public int? prov_ciudId { get; set; }
         public int? prov_paisId { get; set; }
-        public int? coco_Id { get; set; }
-        public string coco_Descripcion { get; set; }
-        public string pvde_Condicion_Otra { get; set; }
-        public int? inte_Id { get; set; }
+
+
         public int? tite_Id { get; set; }
+        public string TipoIntermediario { get; set; }
+
+
+
+        public int? inte_Id { get; set; }
+        public int? inte_ciudId { get; set; }
+        public int? inte_paisId { get; set; }
+        public string inte_Tipo_Otro { get; set; }
+
+
+
         public string inte_NumeroIdentificacion { get; set; }
         public string inte_Nombre_Raso { get; set; }
         public string inte_Direccion_Exacta { get; set; }
         public string inte_Correo_Electronico { get; set; }
         public string inte_Telefono { get; set; }
         public string inte_Fax { get; set; }
-        public int? inte_ciudId { get; set; }
-        public int? inte_paisId { get; set; }
-        public string inte_Tipo_Otro { get; set; }
+
         public string deva_LugarEntrega { get; set; }
         public int? pais_EntregaId { get; set; }
         public string pais_EntregaNombre { get; set; }
@@ -60,19 +91,19 @@ namespace SIMEXPRO.Entities.Entities
         public string inco_Version { get; set; }
         public string deva_NumeroContrato { get; set; }
         public DateTime? deva_FechaContrato { get; set; }
+
+
         public int? foen_Id { get; set; }
         public string foen_Descripcion { get; set; }
         public string deva_FormaEnvioOtra { get; set; }
-        public bool? deva_PagoEfectuado { get; set; }
+        
+
         public int? fopa_Id { get; set; }
+        public string fopa_Descripcion { get; set; }
         public string deva_FormaPagoOtra { get; set; }
-        public int? emba_Id { get; set; }
-        public int? pais_ExportacionId { get; set; }
-        public string pais_ExportacionNombre { get; set; }
-        public DateTime? deva_FechaExportacion { get; set; }
-        public int? mone_Id { get; set; }
-        public string mone_Otra { get; set; }
-        public decimal? deva_ConversionDolares { get; set; }
+        
+     
+      
         public int codi_Id { get; set; }
         public bool codi_Restricciones_Utilizacion { get; set; }
         public string codi_Indicar_Restricciones_Utilizacion { get; set; }
@@ -86,6 +117,9 @@ namespace SIMEXPRO.Entities.Entities
         public string codi_Concepto_Monto_Declarado { get; set; }
         public bool codi_Existen_Canones { get; set; }
         public string codi_Indicar_Canones { get; set; }
+
+
+
         public int base_Id { get; set; }
         public decimal base_PrecioFactura { get; set; }
         public decimal base_PagosIndirectos { get; set; }
@@ -110,6 +144,9 @@ namespace SIMEXPRO.Entities.Entities
         public decimal base_Deducciones_Legales { get; set; }
         public decimal base_Total_Deducciones_Precio { get; set; }
         public decimal base_Valor_Aduana { get; set; }
+
+
+
         public int? usua_UsuarioCreacion { get; set; }
         public string usua_CreacionNombre { get; set; }
         public DateTime? deva_FechaCreacion { get; set; }
