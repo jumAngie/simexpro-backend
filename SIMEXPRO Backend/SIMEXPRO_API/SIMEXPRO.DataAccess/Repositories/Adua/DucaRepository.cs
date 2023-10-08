187487d2f21ae7977813b1238a170bf5d1e6089d
IsHatake <isaacramos191969@gmail.com>
2023-10-08 02:01:33 -0600

    una wea de la duca
    ya ni me acordaba como usar linq

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Adua/DucaRepository.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Adua/DucaRepository.cs
@@ -171,6 +171,12 @@ namespace SIMEXPRO.DataAccess.Repositories.Adua
             return db.Query<tbDuca>(ScriptsDataBase.ListarDuca_ById, parametros, commandType: System.Data.CommandType.StoredProcedure);
         }
 
+        public IEnumerable<tbDuca> List_ByNoDuca(string NoDuca)
+        {
+            using var db = new SqlConnection(SIMEXPRO.ConnectionString);
+            var Query = db.Query<tbDuca>(ScriptsDataBase.ListarDuca, null, commandType: System.Data.CommandType.StoredProcedure);
+            return Query.Where(x => x.duca_No_Duca == NoDuca);
+        }
 
         public IEnumerable<VW_tbDuca_GenerarDuca> generarDuca(int duca_Id)
         {
